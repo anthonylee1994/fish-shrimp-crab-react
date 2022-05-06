abe7b037f209ee8628d710ec9f32fe1696599276
Anthony Lee <hososlch@gmail.com>
2022-05-06 19:57:33 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BetGame from "./artifacts/contracts/bet-game.sol/BetGame.json";
+import { ethers } from "ethers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { ethereum } = window as any;
+export const App = React.memo(() => {
+  const fetchGreetings = React.useCallback(async () => {
+    let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const gasPrice = await provider.getGasPrice();
+    console.log("gasPrice", gasPrice);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, BetGame.abi, signer);
 
-export default App;
+    await contract.bet(true, { value: ethers.utils.parseEther("0.1") });
+  }, []);
+
+  const connectWallet = React.useCallback(async () => {
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      fetchGreetings();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [fetchGreetings]);
+
+  React.useEffect(() => {
+    connectWallet();
+  }, [connectWallet]);
+
+  return <div>111</div>;
+});
